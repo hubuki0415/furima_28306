44b08d54442779e1854e03f16c7411b83a591c5d
hubuki0415 <mangan24@icloud.com>
2020-08-22 16:26:33 +0900

    修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         zennkaku = /\A[ぁ-んァ-ン一-龥]/
+         kana = /\A[ァ-ヶー－]+\z/
+         hankaku = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+         
          with_options presence: true do
-          validates :nickname, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-          validates :first_name, format:  {with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-          validates :last_name, format:  {with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-          validates :first_name_kana, format:  {with: /\A[ァ-ヶー－]+\z/, message: "はカタカナで入力してください。"}
-          validates :last_name_kana, format:  {with: /\A[ァ-ヶー－]+\z/, message: "はカタカナで入力してください。"}
+          validates :nickname, format: {with: zennkaku, message: "は全角で入力してください。"}
+          validates :first_name, format:  {with: zennkaku, message: "は全角で入力してください。"}
+          validates :last_name, format:  {with: zennkaku, message: "は全角で入力してください。"}
+          validates :first_name_kana, format:  {with: kana, message: "はカタカナで入力してください。"}
+          validates :last_name_kana, format:  {with: kana, message: "はカタカナで入力してください。"}
           validates :birth_day
 
         end
-          validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は半角英数字も入れてください。"}
+          validates :password, format: {with: hankaku, message: "は半角英数字も入れてください。"}
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     nickname {"太朗"}
     email {Faker::Internet.free_email}
     password {'1a' + Faker::Internet.password(min_length: 6)}
-    # password {'123456a'}
     password_confirmation {password}
     first_name {"太朗"}
     last_name {"山田"}
